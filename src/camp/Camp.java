8cfeeb4120a89f5b672cc7fb682802b818ed2b85
Serhii Tymoshenko <sergei.timoshenko@pm.me>
2023-10-23 18:30:23 +0200

    Camp added

---
+++ src/camp/Camp.java
@@ -0,0 +1,10 @@
+package camp;
+
+public class Camp {
+    private int startDay;
+    private int endDay;
+    public Camp (int startDay, int endDay) {
+        this.startDay = startDay;
+        this.endDay = endDay;
+    }
+}
